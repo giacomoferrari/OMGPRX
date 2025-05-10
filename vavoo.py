aa3f86bfd248a3e8d08d9b8f6bb0c188d6ea570f
Giacomo Ferrari <giacomoferrari@outlook.it>
2025-05-10 17:22:09 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -8,8 +8,8 @@ PASSWORD = "MediaflowStremio!"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
-USR = "TUO_USER_GITHUB"
-REPO = "TUO_BRANCH_GITHUB"
+USR = "giacomoferrari"
+REPO = "OMGPRX"
 
 OUTPUT_FILE = "channels_italy.m3u8"
 BASE_URL = "https://vavoo.to"
